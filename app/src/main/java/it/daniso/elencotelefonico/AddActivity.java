db4970cd9578cc079388f0311c6100e5db009a5e
daniso96 <daniele.isoni@studenti.unitn.it>
2016-12-13 22:50:24 +0100

    add in progress

--- app/src/main/java/it/daniso/elencotelefonico/AddActivity.java
+++ app/src/main/java/it/daniso/elencotelefonico/AddActivity.java
@@ -3,6 +3,8 @@ package it.daniso.elencotelefonico;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
@@ -32,6 +34,14 @@ public class AddActivity extends AppCompatActivity {
         listDataChild = new HashMap<>();
         listDataChild.put(select, personeList);
         expAdapter = new PersonExpandableListAdapter(this, listDataChild);
+        expView.setAdapter(expAdapter);
+
+        expView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
