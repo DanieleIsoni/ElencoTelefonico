4761fb5fa851dd743d9903a9526b5c5563a12318
daniso96 <daniele.isoni@studenti.unitn.it>
2017-01-23 19:36:31 +0100

    Last changes

--- app/src/main/java/it/daniso/elencotelefonico/DatabaseHandler.java
+++ app/src/main/java/it/daniso/elencotelefonico/DatabaseHandler.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteConstraintException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 import static it.daniso.elencotelefonico.Incarico.incarichi;
 import static it.daniso.elencotelefonico.Incarico.incarichiMap;
